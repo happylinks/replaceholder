1ff8092c23d35f790eb1b15530dc0f424ede4ca4
Alex Komlev <alex@getbynder.com>
2016-11-24 11:44:22 +0100

    AMD support; Options attribute

--- README.md
+++ README.md
@@ -1,13 +1,78 @@
 # replaceholder
-Replace placeholder images with the correct image if it exists.
+Replace placeholders with correct images if they exist.
 
+This is jQuery plugin, so you'll need to have an access to jquery instance on your page or as Require/CommonJS dependency.
 
-```
-Javascript:
+## Example
+
+#### JS:
+```js
 $('.replaceholder').replaceholder();
+```
 
-HTML:
-<img src="/img/placeholders/100x100.png" data-src="source_image.png" alt="" class="replaceholder">
+#### HTML:
+```html
+<img class="replaceholder"
+     src="/img/placeholders/100x100.png"
+     data-src="source_image.png"
+     alt="">
 
-<div class="replaceholder" style="background-image: url('/img/placeholders/640x480.png');" data-type="bg" data-src="source_image.png">
+<div class="replaceholder"
+     style="background-image: url('/img/placeholders/640x480.png');"
+     data-type="bg"
+     data-src="source_image.png">
+
+     =>
+
+<img class="replaceholder"
+     src="source_image.png"
+     data-src="source_image.png"
+     alt="">
+
+<div class="replaceholder"
+     style="background-image: url('source_image.png');"
+     data-type="bg"
+     data-src="source_image.png">
+```
+
+## Options *(since v.0.0.2)*
+
+You can pass options object as parameter to add classes when image will be loaded or return error. This can be helpful if you use fontawesome icons as placeholders. For example:
+
+#### JS:
+```js
+var options = {
+    src: 'newImage.png', // required image url
+    bg: true,            // replaces src attribute (false) or background-image style (true) of the element
+    doneClass: 'done',   // class or classes that will be added to the element when image is loaded
+    failClass: 'failed', // class or classes that will be added to the element if image can't be loaded
+    replaceClass: false  // doneClass and failClass will rewrite (true) or be added (false) current value
+};
+
+$('.thumbnail > li').replaceholder(options);
+```
+
+#### HTML:
+```html
+<ul class="thumbnail">
+    <li>Element 1</li>
+    <li>Element 2</li>
+    <li>Element 3</li>
+</ul>
+
+    =>
+
+<ul class="thumbnail">
+    <li class="failed">Element 1</li>
+    <li class="failed">Element 2</li>
+    <li class="failed">Element 3</li>
+</ul>
+
+    or
+
+<ul class="thumbnail">
+    <li class="done" style="background-image: url(newImage.png);">Element 1</li>
+    <li class="done" style="background-image: url(newImage.png);">Element 2</li>
+    <li class="done" style="background-image: url(newImage.png);">Element 3</li>
+</ul>
 ```
--- replaceholder.js
+++ replaceholder.js
@@ -1,23 +1,64 @@
-(function ($) {
-    $.fn.replaceholder = function () {
-        return this.each(function () {
-            var img = $(this);
-            var src = img.data('src');
-            var type = img.data('type');
+(function (global, factory) {
+    'use strict';
 
-            var new_img = new Image();
-            new_img.onload = function () {
-                if (type === 'bg'){
-                    img.css('background-image', 'url("' + this.src + '")');
-                } else {
-                    img.attr('src', this.src);
-                }
-                delete new_img;
-            };
-            if (src){
-                new_img.src = src;
+    if (typeof define === 'function' && define.amd) {
+        // AMD require.js
+        define('replaceholder', ['jquery'], function ($) {
+            $.fn.replaceholder = factory();
+        });
+    } else if (typeof exports !== 'undefined') {
+        // Node.js CommonJS
+        require('jquery').fn.replaceholder = factory();
+    } else if (typeof global.$ === 'function' && typeof global.$.fn === 'object') {
+        // Global scope
+        global.$.fn.replaceholder = factory();
+    }
+}(this, function () {
+    'use strict';
+
+    // options argument is an object that may contain:
+    // src: 'newImage.png' // required image url
+    // bg: false           // replaces src attribute (false) or background-image style (true) of the element
+    // doneClass: 'string' // class or classes that will be added to the element when image is loaded
+    // failClass: 'string' // class or classes that will be added to the element if image can't be loaded
+    // replaceClass: false // doneClass and failClass will rewrite (true) or be added (false) current value
+    return function (options) {
+        options = options || {};
+        return this.each(function () {
+            var img = this;
+            var src = options.src || img.getAttribute('data-src');
+            if (!src) {
+                return;
             }
+            var background = options.bg || img.getAttribute('data-type') === 'bg' ||
+                img.tagName.toLowerCase() !== 'img';
+            var doneClass = (typeof options.doneClass === 'string') ? options.doneClass : undefined;
+            var failClass = (typeof options.failClass === 'string') ? options.failClass : undefined;
+
+            var changeClass = function (newClass) {
+                if (!newClass) {
+                    return;
+                }
+                if (options.replaceClass) {
+                    img.setAttribute('class', newClass);
+                } else {
+                    img.className = (img.className + ' ' + newClass).trim();
+                }
+            };
+
+            var tempImg = new Image();
+            tempImg.onload = function () {
+                if (background) {
+                    img.style.backgroundImage = 'url("' + this.src + '")';
+                } else {
+                    img.setAttribute('src', this.src);
+                }
+                changeClass(doneClass);
+            };
+            tempImg.onerror = function () {
+                changeClass(failClass);
+            };
+            tempImg.src = src;
         });
     };
-}(jQuery));
-
+}));
